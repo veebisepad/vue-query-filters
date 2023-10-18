69956f0cde731452a6ed05eb46f071c8362f2b10
ralf <ralf@veebisepad.ee>
2023-10-18 11:31:29 +0300

    Code improvements

--- index.ts
+++ index.ts
@@ -1,20 +1,8 @@
 import { reactive } from 'vue'
 
-interface filterObject {
-    key: string,
-    multiple: boolean
-}
+export type QueryObject = Record<string | symbol, string>
 
-interface options {
-    delimiter?: string
-    callbackFn?(filters: QueryObject): void
-}
-export type FilterValue = string[] | number[] | string | number | boolean | null
-export type FilterStruct = filterObject
-export type QueryObject = Record<string, string>
-export type FilterValueObject = Record<string, FilterValue>
-
-interface FilterParams {
+interface filterParams {
     toSeachParams(): URLSearchParams
     toQueryObject(): QueryObject
     has(filter: string, value: string | number): boolean
@@ -22,29 +10,43 @@ interface FilterParams {
     get(): void
     clearAll(): void
 }
-export type Options<options> = options
-export type FilterOptions<filterValueObject> = filterValueObject & FilterParams
 
-export function useFilters(filters: filterObject[], options: Options<options>): FilterOptions<FilterValueObject> {
+interface options {
+    delimiter?: string
+    callbackFn?(filters: QueryObject): void
+}
+
+interface filterObject {
+    key: string,
+    multiple: boolean
+}
+
+
+export type FilterStruct = filterObject
+export type Options = options
+export type FilterOptions = filterParams & FilterValueObject
+export type FilterValueObject = Record<string | symbol, any>
+
+export function useFilters(filters: FilterStruct[], options: Options = {}): FilterOptions {
     const queryParams = new URLSearchParams(document.location.search)
     const delimiter = options.delimiter ?? ','
 
-    let filterParams: FilterValueObject = {}
+    let filterKeys: FilterValueObject = {}
 
     filters.forEach((filter: FilterStruct) => {
         const value = queryParams.get(filter.key)
         if(value){
-            filterParams[filter.key] = filter.multiple ? value.split(delimiter) : value
+            filterKeys[filter.key] = filter.multiple ? value.split(delimiter) : value
         }
         else {
-            filterParams[filter.key] = filter.multiple ? [] : null
+            filterKeys[filter.key] = filter.multiple ? [] : null
         }
     })
     const params = reactive({
-        ...filterParams,
-        hasCallback: options.callbackFn ? true : false,
+        ...filterKeys,
+        hasCallback: !!options.callbackFn,
 
-        toSeachParams() {
+        toSeachParams(): URLSearchParams {
             return new URLSearchParams(this.toQueryObject())
         },
         get(): void {
@@ -60,10 +62,10 @@ export function useFilters(filters: filterObject[], options: Options<options>): 
 
             filters.forEach(filter => {
                 if(Array.isArray(this[filter.key]) && this[filter.key].length){
-                  return queryObject[filter.key] = this[filter.key].join(delimiter)
+                  queryObject[filter.key] = this[filter.key].join(delimiter)
                 }
                 else if(this[filter.key]){
-                    return queryObject[filter.key] = this[filter.key]
+                   queryObject[filter.key] = this[filter.key]
                 }
             })
             return queryObject
